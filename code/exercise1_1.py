9a351596f6993e2cbbaa52f2f87919fe08d6bd89
정태한 <117992107+timkorea0810@users.noreply.github.com>
2025-09-11 17:15:53 +0900

    Add files via upload

---
+++ code/exercise1_1.py
@@ -0,0 +1,26 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+v1 = Vector(1, 2)
+v2 = Vector(3, 4)
+print(v1 + v2)
+print(v1 - v2)
+print(v1 == v1)
+print(v1 == v2)
+print(v1 * 2)
